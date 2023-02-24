ca7a8fe65163daf81a3f04d47d473659d1e56b74
ariel.olin@apiiro.com <ariel0lin@gmail.com>
2023-02-25 00:40:15 +0200

    eq

--- controllers/time-controller.js
+++ controllers/time-controller.js
@@ -6,7 +6,7 @@ export const getDailyGroupInfo = async (date, groupId) => {
   console.log(date, group);
   const result = await dayModel.findOne(
     {
-      date: new Date(date).setHours(0, 0, 0, 0),
+      date: { $eq: new Date(date).setHours(0, 0, 0, 0) },
       groups: { $elemMatch: { name: group.name } },
     },
     { "groups.$": 1 }
